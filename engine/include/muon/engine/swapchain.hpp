d582052c27e0bed0b13b7250bc699f61b3b88522
Liam <lima.mail725@gmail.com>
2025-04-13 22:51:50 +1000

    Added documentation.

--- engine/include/muon/engine/swapchain.hpp
+++ engine/include/muon/engine/swapchain.hpp
@@ -24,82 +24,106 @@ namespace muon::engine {
         Swapchain(const Swapchain &) = delete;
         Swapchain &operator=(const Swapchain &) = delete;
 
+        /**
+         * @brief   acquires the next image in the swapchain.
+         *
+         * @param   imageIndex  pointer to integer.
+         *
+         * @return  result of the process.
+         */
         [[nodiscard]] vk::Result acquireNextImage(uint32_t *imageIndex);
+
+        /**
+         * @brief   submits command buffer for drawing.
+         *
+         * @param   buffers     pointer to command buffer.
+         * @param   imageIndex  pointer to integer.
+         *
+         * @return  result of the process.
+         */
         [[nodiscard]] vk::Result submitCommandBuffers(const vk::CommandBuffer *buffers, uint32_t *imageIndex);
+
+        /**
+         * @brief   compares swapchain image formats.
+         *
+         * @param   swapchain   the other swapchain to compare against.
+         *
+         * @return  whether they are the same.
+         */
         [[nodiscard]] bool compareSwapFormats(const Swapchain &swapchain) const;
 
         /**
-         * @brief   Gets the number of images in the swapchain.
+         * @brief   gets the number of images in the swapchain.
          *
          * @return  swapchain image count.
-        */
+         */
         [[nodiscard]] size_t imageCount() const;
 
         /**
-         * @brief   Returns the swapchain.
+         * @brief   returns the swapchain.
          *
          * @return  swapchain handle.
-        */
+         */
         [[nodiscard]] vk::SwapchainKHR getSwapchain() const;
 
         /**
-         * @brief   Returns the render pass.
+         * @brief   returns the render pass.
          *
          * @return  render pass handle.
-        */
+         */
         [[nodiscard]] vk::RenderPass getRenderPass() const;
 
         /**
-         * @brief   Returns framebuffer at image index.
+         * @brief   returns framebuffer at image index.
          *
          * @param   index   the index of the swapchain framebuffer.
          *
          * @return  framebuffer at index.
-        */
+         */
         [[nodiscard]] vk::Framebuffer getFramebuffer(int32_t index) const;
 
         /**
-         * @brief   Returns image view at image index.
+         * @brief   returns image view at image index.
          *
          * @param   index   the index of the swapchain image view.
          *
          * @return  image view at index.
-        */
+         */
         [[nodiscard]] vk::ImageView getImageView(int32_t index) const;
 
         /**
-         * @brief   Returns image format of the swapchain.
+         * @brief   returns image format of the swapchain.
          *
          * @return  image format.
-        */
+         */
         [[nodiscard]] vk::Format getSwapchainImageFormat() const;
 
         /**
-         * @brief   Returns the two dimensional size of the swapchain.
+         * @brief   returns the two dimensional size of the swapchain.
          *
          * @return  2d extent of the swapchain.
-        */
+         */
         [[nodiscard]] vk::Extent2D getExtent() const;
 
         /**
-         * @brief   Returns the width of the swapchain images.
+         * @brief   returns the width of the swapchain images.
          *
          * @return  image width.
-        */
+         */
         [[nodiscard]] uint32_t getWidth() const;
 
         /**
-         * @brief   Returns the height of the swapchain images.
+         * @brief   returns the height of the swapchain images.
          *
          * @return  image height.
-        */
+         */
         [[nodiscard]] uint32_t getHeight() const;
 
         /**
-         * @brief   Returns the aspect ratio of the swapchain images.
+         * @brief   returns the aspect ratio of the swapchain images.
          *
          * @return  aspect ratio.
-        */
+         */
         [[nodiscard]] float getExtentAspectRatio() const;
 
     private:
@@ -107,62 +131,62 @@ namespace muon::engine {
         vk::Extent2D windowExtent;
 
         vk::SwapchainKHR swapchain;
-        std::shared_ptr<Swapchain> oldSwapchain;
+        std::shared_ptr<Swapchain> oldSwapchain = nullptr;
         vk::Extent2D swapchainExtent;
 
         vk::Format swapchainImageFormat;
-        std::vector<vk::Image> swapchainImages;
-        std::vector<vk::ImageView> swapchainImageViews;
+        std::vector<vk::Image> swapchainImages{};
+        std::vector<vk::ImageView> swapchainImageViews{};
 
         vk::Format depthImageFormat;
-        std::vector<vk::Image> depthImages;
-        std::vector<vma::Allocation> depthImageAllocations;
-        std::vector<vk::ImageView> depthImageViews;
+        std::vector<vk::Image> depthImages{};
+        std::vector<vma::Allocation> depthImageAllocations{};
+        std::vector<vk::ImageView> depthImageViews{};
 
         vk::RenderPass renderPass;
 
-        std::vector<vk::Framebuffer> swapchainFramebuffers;
+        std::vector<vk::Framebuffer> swapchainFramebuffers{};
 
-        std::vector<vk::Semaphore> imageAvailableSemaphores;
-        std::vector<vk::Semaphore> renderFinishedSemaphores;
-        std::vector<vk::Fence> inFlightFences;
-        std::vector<vk::Fence> imagesInFlight;
+        std::vector<vk::Semaphore> imageAvailableSemaphores{};
+        std::vector<vk::Semaphore> renderFinishedSemaphores{};
+        std::vector<vk::Fence> inFlightFences{};
+        std::vector<vk::Fence> imagesInFlight{};
 
-        int32_t currentFrame = 0;
+        int32_t currentFrame{0};
 
         /**
-         * @brief   Initialises the swapchain.
-        */
+         * @brief   initialises the swapchain.
+         */
         void init();
 
         /**
-         * @brief   Creates the swapchain.
-        */
+         * @brief   creates the swapchain.
+         */
         void createSwapchain();
 
         /**
-         * @brief   Creates all the image views.
-        */
+         * @brief   creates all the image views.
+         */
         void createImageViews();
 
         /**
-         * @brief   Creates the depth buffer resources.
-        */
+         * @brief   creates the depth buffer resources.
+         */
         void createDepthResources();
 
         /**
-         * @brief   Creates the render pass.
-        */
+         * @brief   creates the render pass.
+         */
         void createRenderPass();
 
         /**
-         * @brief   Creates all the framebuffers.
-        */
+         * @brief   creates all the framebuffers.
+         */
         void createFramebuffers();
 
         /**
-         * @brief   Creates the synchronisation objects.
-        */
+         * @brief   creates the synchronisation objects.
+         */
         void createSyncObjects();
 
     };
