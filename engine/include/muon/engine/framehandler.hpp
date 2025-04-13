70c4ea0bc6fedc01eafc3b51812ff0bc661f0e96
Liam <lima.mail725@gmail.com>
2025-04-13 14:52:33 +1000

    Added documentation to frame handler class.

--- engine/include/muon/engine/framehandler.hpp
+++ engine/include/muon/engine/framehandler.hpp
@@ -9,6 +9,9 @@
 
 namespace muon::engine {
 
+    /**
+     * @brief   handles the swapchain, recording commands, and presenting frames.
+     */
     class FrameHandler {
     public:
         FrameHandler(Window &window, Device &device);
@@ -17,17 +20,79 @@ namespace muon::engine {
         FrameHandler(const FrameHandler &) = delete;
         FrameHandler &operator=(const FrameHandler &) = delete;
 
+        /**
+         * @brief   provides a command buffer to begin recording commands.
+         *
+         * @return  command buffer to record commands into for the current frame.
+         */
         [[nodiscard]] vk::CommandBuffer beginFrame();
+
+        /**
+         * @brief   ends the current frame being recorded.
+         */
         void endFrame();
+
+        /**
+         * @brief   begins the swapchain render pass.
+         *
+         * @param   commandBuffer   the command buffer currently being recorded into for the frame.
+         */
         void beginSwapchainRenderPass(vk::CommandBuffer commandBuffer);
+
+        /**
+         * @brief   ends the swapchain render pass.
+         *
+         * @param   commandBuffer   the command buffer currently being recorded into for the frame.
+         */
         void endSwapchainRenderPass(vk::CommandBuffer commandBuffer);
 
+        /**
+         * @brief   gets the render pass from the swapchain.
+         *
+         * @return  rende pass handle.
+         */
         [[nodiscard]] vk::RenderPass getSwapchainRenderPass() const;
+
+        /**
+         * @brief   gets the current command buffer.
+         *
+         * @return  command buffer handle.
+         */
         [[nodiscard]] vk::CommandBuffer getCurrentCommandBuffer() const;
+
+        /**
+         * @brief   sets the swapchain clear color.
+         *
+         * @param   new color to set the background to.
+         */
         void setClearColor(vk::ClearColorValue newValue);
+
+        /**
+         * @brief   sets the swapchain clear depth stencil.
+         *
+         * @param   new depth stencil value to set.
+         */
         void setClearDepthStencil(vk::ClearDepthStencilValue newValue);
+
+        /**
+         * @brief   gets the current frame index.
+         *
+         * @return  frame index.
+         */
         [[nodiscard]] int32_t getFrameIndex() const;
+
+        /**
+         * @brief   gets the state of frame.
+         *
+         * @return  whether the frame is in progress.
+         */
         [[nodiscard]] bool isFrameInProgress() const;
+
+        /**
+         * @brief   gets the aspect ratio of the swapchain image.
+         *
+         * @return  aspect ratio.
+         */
         [[nodiscard]] float getAspectRatio() const;
 
     private:
@@ -44,8 +109,19 @@ namespace muon::engine {
         int32_t currentFrameIndex{0};
         bool frameInProgress{false};
 
+        /**
+         * @brief   creates command buffers for each image in the swapchain.
+         */
         void createCommandBuffers();
+
+        /**
+         * @brief   frees the allocated command buffers.
+         */
         void freeCommandBuffers();
+
+        /**
+         * @brief   recreates the swapchain, used for when window size changes, etc.
+         */
         void recreateSwapchain();
     };
 
