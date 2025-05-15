1209e2ecd253101423add23e5148f53296123758
Liam <lima.mail725@gmail.com>
2025-05-15 12:54:49 +1000

    Basic directed acyclic graph topographical sorting using Kahn's algorithm.

--- engine/include/muon/engine/rendergraph.hpp
+++ engine/include/muon/engine/rendergraph.hpp
@@ -6,75 +6,31 @@
 #include <unordered_map>
 #include <vulkan/vulkan.hpp>
 
-namespace muon::engine {
-    class Device;
-    class Image;
-}
+// namespace muon::engine {
+//     class Device;
+//     class Image;
+// }
 
 namespace muon::engine::rg {
 
-    enum class ResourceOp {
-        Read,
-        Write,
-    };
-
-    struct ResourceConfig {
-        vk::Format format;
-        vk::ImageLayout layout;
-        vk::ImageUsageFlags usageFlags;
-        vk::AccessFlags2 accessFlags;
-        vk::PipelineStageFlags2 stageFlags;
-    };
-
-    struct ResourceUsage {
-        std::string name;
-        ResourceOp op;
-        vk::ImageLayout layout;
-        vk::AccessFlags2 accessFlags;
-        vk::PipelineStageFlags2 stageFlags;
-    };
-
-    enum class NodeType {
-        Render,
-        PostProcessing,
-        Transfer,
-        Presentation,
-    };
-
     struct Node {
         std::string name;
-        NodeType type;
-
-        std::vector<ResourceUsage> resources{};
-
-        std::vector<vk::ImageMemoryBarrier2> preBarriers{};
-        std::vector<vk::ImageMemoryBarrier2> postBarriers{};
+        std::vector<std::string> readDeps;
+        std::vector<std::string> writeDeps;
     };
 
     class RenderGraph {
     public:
-        RenderGraph(Device &device);
+        RenderGraph();
         ~RenderGraph();
 
-        void setImageSize(const vk::Extent2D &imageSize);
-
-        void addResource(const std::string &name, const ResourceConfig &resource);
-
-        void addNode(const Node &node);
+        void addNode(Node node);
 
         void compile();
-
         void execute();
 
     private:
-        Device &device;
-
-        vk::Extent2D imageSize;
-
-        std::unordered_map<std::string, ResourceConfig> resourceBlueprints;
-        std::unordered_map<std::string, std::unique_ptr<Image>> resources;  // is populated upon compilation
-
-        std::vector<Node> nodes;
+        std::unordered_map<std::string, Node> nodes;
 
     };
 
--- engine/src/engine/rendergraph.cpp
+++ engine/src/engine/rendergraph.cpp
@@ -1,39 +1,86 @@
 #include "muon/engine/rendergraph.hpp"
 
+#include <queue>
+#include <stdexcept>
+#include <unordered_set>
+#include <algorithm>
+
 #include "muon/log/logger.hpp"
 #include "muon/engine/device.hpp"
 #include "muon/engine/image.hpp"
 
 namespace muon::engine::rg {
 
-    RenderGraph::RenderGraph(Device &device) : device(device) {}
+    RenderGraph::RenderGraph()  {}
 
     RenderGraph::~RenderGraph() {}
 
-    void RenderGraph::setImageSize(const vk::Extent2D &imageSize) {
-        this->imageSize = imageSize;
-    }
-
-    void RenderGraph::addResource(const std::string &name, const ResourceConfig &resource) {
-        resourceBlueprints[name] = resource;
-    }
-
-    void RenderGraph::addNode(const Node &node) {
-        nodes.push_back(node);
+    void RenderGraph::addNode(Node node) {
+        nodes[node.name] = node;
     }
 
     void RenderGraph::compile() {
-        for (const auto &[name, resource] : resourceBlueprints) {
-            log::globalLogger->info("creating image {}", name);
-            resources[name] = engine::Image::Builder(device)
-                .setExtent(imageSize)
-                .setFormat(resource.format)
-                .setImageLayout(resource.layout)
-                .setImageUsageFlags(resource.usageFlags)
-                .setAccessFlags(resource.accessFlags)
-                .setPipelineStageFlags(resource.stageFlags)
-                .buildUniquePtr();
+
+        std::unordered_map<std::string, std::unordered_set<std::string>> dependencies;
+
+        for (const auto &[_, node] : nodes) {
+            dependencies[node.name];
+
+            for (const auto &[_, other] : nodes) {
+
+                if (node.name == other.name) {
+                    continue;
+                }
+
+                auto &readDeps = node.readDeps;
+                for (auto &readDep : readDeps) {
+
+                    auto it = std::find_if(
+                        other.writeDeps.begin(),
+                        other.writeDeps.end(),
+                        [&readDep](const auto &writeDep) {
+                            return readDep == writeDep;
+                        }
+                    );
+                    if (it == other.writeDeps.end()) {
+                        continue;
+                    }
+
+                    dependencies[node.name].insert(other.name);
+                }
+            }
         }
+
+        std::unordered_map<std::string, uint32_t> inDegrees;
+        std::queue<std::string> queue;
+
+        for (const auto &[node, deps] : dependencies) {
+            inDegrees[node] = deps.size();
+            if (inDegrees[node] == 0) {
+                queue.push(node);
+            }
+        }
+
+        std::vector<std::string> order;
+
+        while (!queue.empty()) {
+            auto node = queue.front();
+            queue.pop();
+            order.push_back(node);
+
+            for (auto &[dep, deps] : dependencies) {
+                if (deps.contains(node)) {
+                    inDegrees[dep] -= 1;
+                    if (inDegrees[dep] == 0) {
+                        queue.push(dep);
+                    }
+                }
+            }
+        }
+    }
+
+    void RenderGraph::execute() {
+
     }
 
 }
