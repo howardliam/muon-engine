40dd673e908671b6a60fd01fe24e7b411ae94b03
Liam <lima.mail725@gmail.com>
2025-05-19 11:12:41 +1000

    Some clean up.

--- engine/include/muon/engine/frame_graph/frame_graph.hpp
+++ engine/include/muon/engine/frame_graph/frame_graph.hpp
@@ -2,68 +2,30 @@
 
 #include "muon/utils/nocopy.hpp"
 #include "muon/utils/nomove.hpp"
-#include <unordered_map>
-#include <unordered_set>
-#include <string>
-#include <functional>
-#include <vector>
-#include <memory>
-
-namespace {
-    using DependencyMap = std::unordered_map<std::string, std::unordered_set<std::string>>;
-}
-
-namespace muon::engine {
-    class Device;
-    class Image;
-}
 
 namespace muon::engine::fg {
-
-    class ResourceBuilder;
-
-    struct Node {
-        std::string name;
-        std::vector<std::string> readDeps;
-        std::vector<std::string> writeDeps;
-    };
 
     class FrameGraph : NoCopy, NoMove {
     public:
         class Builder;
 
-        FrameGraph(Device &device);
+        FrameGraph();
         ~FrameGraph();
-
-        void configureResources(std::function<void(Builder &)> callback);
-
-        void addNode(Node node);
 
         void compile();
 
         void execute();
 
     private:
-        Device &device;
 
-        std::unordered_map<std::string, std::unique_ptr<Image>> images;
-
-        std::unordered_map<std::string, Node> nodes;
-        std::vector<std::string> order;
-
-        DependencyMap determineDependencies(const std::unordered_map<std::string, Node> &nodes);
-
-        std::vector<std::string> topographicalSort(const DependencyMap &dependencies);
     };
 
     class FrameGraph::Builder : NoCopy, NoMove {
     public:
 
-        void addImage(const std::string &name);
+
 
     private:
-
-
 
         friend class FrameGraph;
     };
--- engine/src/engine/frame_graph/frame_graph.cpp
+++ engine/src/engine/frame_graph/frame_graph.cpp
@@ -1,73 +1,7 @@
 #include "muon/engine/frame_graph/frame_graph.hpp"
 
-#include "muon/engine/image.hpp"
-#include <queue>
-
 namespace muon::engine::fg {
 
-    FrameGraph::FrameGraph(Device &device) : device(device) {
 
-    }
-
-    FrameGraph::~FrameGraph() {
-
-    }
-
-    DependencyMap FrameGraph::determineDependencies(const std::unordered_map<std::string, Node> &nodes) {
-        DependencyMap dependencies;
-
-        for (const auto &[_, node] : nodes) {
-            dependencies[node.name];
-
-            for (const auto &[_, other] : nodes) {
-                if (node.name == other.name) { continue; }
-
-                auto &readDeps = node.readDeps;
-                for (auto &readDep : readDeps) {
-                    auto predicate = [&readDep](const auto &writeDep) { return readDep == writeDep; };
-                    auto it = std::find_if(other.writeDeps.begin(), other.writeDeps.end(), predicate);
-
-                    if (it == other.writeDeps.end()) { continue; }
-
-                    dependencies[node.name].insert(other.name);
-                }
-            }
-        }
-
-        return dependencies;
-    }
-
-    std::vector<std::string> FrameGraph::topographicalSort(const DependencyMap &dependencies) {
-        std::unordered_map<std::string, uint32_t> inDegrees;
-        std::queue<std::string> queue;
-
-        for (const auto &[node, deps] : dependencies) {
-            inDegrees[node] = deps.size();
-            if (inDegrees[node] == 0) { queue.push(node); }
-        }
-
-        size_t index{0};
-        std::vector<std::string> order(dependencies.size());
-
-        while (!queue.empty()) {
-            if (index >= order.size()) {
-                throw std::runtime_error("cycle detected");
-            }
-
-            auto node = queue.front();
-            queue.pop();
-
-            order[index++] = node;
-
-            for (auto &[dep, deps] : dependencies) {
-                if (!deps.contains(node)) { continue; }
-
-                inDegrees[dep] -= 1;
-                if (inDegrees[dep] == 0) { queue.push(dep); }
-            }
-        }
-
-        return order;
-    }
 
 }
