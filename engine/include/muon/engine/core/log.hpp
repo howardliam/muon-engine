b9f565d1530550590bb92cbdf0518b87a8acd8de
Liam <lima.mail725@gmail.com>
2025-05-24 12:37:29 +1000

    Updated log to reflect new debug macro.

--- engine/include/muon/engine/core/log.hpp
+++ engine/include/muon/engine/core/log.hpp
@@ -21,15 +21,29 @@ namespace mu {
 
 }
 
-#define MU_CORE_TRACE(...)      mu::Log::getCoreLogger()->trace(__VA_ARGS__)
-#define MU_CORE_DEBUG(...)      mu::Log::getCoreLogger()->debug(__VA_ARGS__)
+#ifdef MU_DEBUG_ENABLED
+
+    #define MU_CORE_TRACE(...)      mu::Log::getCoreLogger()->trace(__VA_ARGS__)
+    #define MU_CORE_DEBUG(...)      mu::Log::getCoreLogger()->debug(__VA_ARGS__)
+
+    #define MU_TRACE(...)           mu::Log::getClientLogger()->trace(__VA_ARGS__)
+    #define MU_DEBUG(...)           mu::Log::getClientLogger()->debug(__VA_ARGS__)
+
+#else
+
+    #define MU_CORE_TRACE(...)
+    #define MU_CORE_DEBUG(...)
+
+    #define MU_TRACE(...)
+    #define MU_DEBUG(...)
+
+#endif
+
 #define MU_CORE_INFO(...)       mu::Log::getCoreLogger()->info(__VA_ARGS__)
 #define MU_CORE_WARN(...)       mu::Log::getCoreLogger()->warn(__VA_ARGS__)
 #define MU_CORE_ERROR(...)      mu::Log::getCoreLogger()->error(__VA_ARGS__)
 #define MU_CORE_CRITICAL(...)   mu::Log::getCoreLogger()->critical(__VA_ARGS__)
 
-#define MU_TRACE(...)           mu::Log::getClientLogger()->trace(__VA_ARGS__)
-#define MU_DEBUG(...)           mu::Log::getClientLogger()->debug(__VA_ARGS__)
 #define MU_INFO(...)            mu::Log::getClientLogger()->info(__VA_ARGS__)
 #define MU_WARN(...)            mu::Log::getClientLogger()->warn(__VA_ARGS__)
 #define MU_ERROR(...)           mu::Log::getClientLogger()->error(__VA_ARGS__)
