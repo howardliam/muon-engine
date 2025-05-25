17affec75253e8adeb6d994ffadc0ec4252ace94
Liam <lima.mail725@gmail.com>
2025-05-25 16:56:24 +1000

    fix: added logging for creation and destruction of the profiler context.

--- engine/src/engine/debug/profiler.cpp
+++ engine/src/engine/debug/profiler.cpp
@@ -1,5 +1,7 @@
 #include "muon/engine/debug/profiler.hpp"
 
+#include "muon/engine/core/log.hpp"
+
 namespace muon {
 
     tracy::VkCtx *Profiler::s_tracyContext{nullptr};
@@ -14,10 +16,12 @@ namespace muon {
 
     void Profiler::createContext(vk::PhysicalDevice pd, vk::Device d, vk::Queue gq, vk::CommandBuffer cmd) {
         s_tracyContext = TracyVkContext(pd, d, gq, cmd);
+        MU_CORE_DEBUG("created profiler context");
     }
 
     void Profiler::destroyContext() {
         TracyVkDestroy(s_tracyContext);
+        MU_CORE_DEBUG("destroyed profiler context");
     }
 
 }
