93fea82f679ef47eeacc698b471d51fe4ff1fd0c
Liam <lima.mail725@gmail.com>
2025-05-27 02:05:49 +1000

    fix: migrate window2 into new file, preparation for switching to GLFW
    Moving Window2 class into a new header in engine/core, getting ready to migrate to GLFW.

--- engine/include/muon/engine/platform/window.hpp
+++ engine/include/muon/engine/platform/window.hpp
@@ -9,40 +9,6 @@
 
 namespace muon {
 
-    class Window2 {
-    public:
-        struct Properties {
-            std::string title;
-            uint32_t width;
-            uint32_t height;
-        };
-
-        Window2();
-        ~Window2();
-
-        [[nodiscard]] bool createSurface(VkInstance instance, VkSurfaceKHR *surface);
-        [[nodiscard]] std::vector<const char *> requiredExtensions() const;
-
-        [[nodiscard]] vk::Extent2D extent() const;
-        [[nodiscard]] uint32_t width() const;
-        [[nodiscard]] uint32_t height() const;
-
-    private:
-        void initSdl();
-        void initWindow(const std::string_view title);
-
-    private:
-        SDL_Window *m_window;
-
-        struct Data {
-            std::string title;
-            uint32_t width;
-            uint32_t height;
-        };
-
-        Data m_data;
-    };
-
     class Window : NoCopy, NoMove {
     public:
         enum class DisplayMode;
