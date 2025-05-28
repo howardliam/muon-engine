b25f3df34e72d91856dd9019f5007c6c34075a76
Liam <lima.mail725@gmail.com>
2025-05-28 23:35:32 +1000

    feat: remove commonlib
    No longer in use, compression will eventually be integrated directly into the engine.

--- common/src/log.cpp
+++
@@ -1,9 +0,0 @@
-#include "muon/common/log.hpp"
-
-namespace mu::common::log {
-
-    std::optional<int32_t> deleteOldLogs(std::filesystem::path directory, size_t maxHistory, std::chrono::day maxAge) {
-        return {};
-    }
-
-}
