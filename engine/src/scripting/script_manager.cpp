a2c053fe0a5e22cb13cfe48f39e16faf1f00c8d6
Liam <lima.mail725@gmail.com>
2025-05-29 12:28:33 +1000

    feat: loading custom function into Lua

--- engine/src/scripting/script_manager.cpp
+++ engine/src/scripting/script_manager.cpp
@@ -1,9 +1,18 @@
 #include "muon/scripting/script_manager.hpp"
 
+#include "muon/core/log.hpp"
+
 namespace muon {
 
     ScriptManager::ScriptManager() {
         m_lua.open_libraries(sol::lib::base, sol::lib::package);
+
+        auto test = []() {
+            MU_INFO("hello from Lua");
+        };
+
+        m_lua.set_function("hello", test);
+
         m_script = m_lua.load_file("assets/scripts/test.lua");
     }
 
