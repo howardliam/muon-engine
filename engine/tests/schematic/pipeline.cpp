068277772eb8a65722140c0730bdfbe9477f238d
Liam <lima.mail725@gmail.com>
2025-07-02 18:48:09 +1000

    fix: update tests

--- engine/tests/schematic/pipeline.cpp
+++ engine/tests/schematic/pipeline.cpp
@@ -1,17 +1,80 @@
 #include "muon/schematic/pipeline.hpp"
-#include "muon/schematic/shader.hpp"
 
 #include <catch2/catch_test_macros.hpp>
-#include <iostream>
 
 namespace muon::schematic {
 
     using json = nlohmann::json;
     using namespace nlohmann::literals;
 
+    const auto jsonMeshletPipeline = R"({
+        "shaders": {
+            "Fragment": {
+                "entryPoint": "main",
+                "path": "foo"
+            },
+            "Mesh": {
+                "entryPoint": "main",
+                "path": "foo",
+                "workGroupSize": [
+                    32,
+                    32,
+                    1
+                ]
+            },
+            "Task": {
+                "entryPoint": "main",
+                "path": "foo"
+            }
+        },
+        "state": {
+            "colorBlend": {
+                "blendConstants": [
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0
+                ],
+                "logicOpEnable": false
+            },
+            "depthStencil": {
+                "depthTestEnable": false,
+                "stencilTestEnable": false
+            },
+            "dynamic": {
+                "states": [
+                    0,
+                    1
+                ]
+            },
+            "multisample": {
+                "alphaToCoverageEnable": false,
+                "alphaToOneEnable": false,
+                "rasterizationSamples": 0,
+                "sampleShadingEnable": false
+            },
+            "rasterization": {
+                "cullMode": 2,
+                "depthBiasClamp": 0.0,
+                "depthBiasConstantFactor": 0.0,
+                "depthBiasEnable": false,
+                "depthBiasSlopeFactor": 0.0,
+                "depthClampEnable": false,
+                "frontFace": 1,
+                "polygonMode": 0,
+                "rasterizerDiscardEnable": false
+            },
+            "viewport": {
+                "scissorCount": 1,
+                "viewportCount": 1
+            }
+        },
+        "type": "Meshlet"
+    })"_json;
+
     TEST_CASE("pipeline serialization", "[schematic]") {
         Pipeline pipeline{};
-        pipeline.type = PipelineType::Compute;
+        pipeline.type = PipelineType::Meshlet;
 
         pipeline.shaders[ShaderStage::Task].entryPoint = "main";
         pipeline.shaders[ShaderStage::Task].path = "foo";
@@ -25,13 +88,24 @@ namespace muon::schematic {
         pipeline.shaders[ShaderStage::Fragment].path = "foo";
         pipeline.shaders[ShaderStage::Fragment].workGroupSize = std::nullopt;
 
+        pipeline.state.emplace();
+
+        pipeline.state->viewport.viewportCount = 1;
+        pipeline.state->viewport.scissorCount = 1;
+
+        pipeline.state->rasterization.cullMode = CullMode::Back;
+        pipeline.state->rasterization.polygonMode = PolygonMode::Fill;
+        pipeline.state->rasterization.frontFace = FrontFace::Clockwise;
+
+        pipeline.state->dynamic.states.push_back(DynamicState::Viewport);
+        pipeline.state->dynamic.states.push_back(DynamicState::Scissor);
+
         const json j = pipeline;
-
-        std::cout << j.dump(4) << '\n';
-
+        REQUIRE(j == jsonMeshletPipeline);
     }
 
     TEST_CASE("pipeline deserialization", "[schematic]") {
+
     }
 
 }
