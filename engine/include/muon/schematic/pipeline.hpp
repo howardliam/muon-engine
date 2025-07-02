93a9eaa3df9146a78871c26f2124353b25a9d884
Liam <lima.mail725@gmail.com>
2025-07-02 18:39:35 +1000

    fix: refactored serialisation

--- engine/include/muon/schematic/pipeline.hpp
+++ engine/include/muon/schematic/pipeline.hpp
@@ -1,15 +1,13 @@
 #pragma once
 
-#include "muon/core/assert.hpp"
-#include "muon/schematic/pipeline_state.hpp"
+#include "muon/schematic/pipeline_state_info.hpp"
 #include "muon/schematic/shader.hpp"
 #include <cstdint>
-#include <fmt/format.h>
-#include <map>
+#include <magic_enum/magic_enum.hpp>
 #include <nlohmann/json.hpp>
 #include <nlohmann/adl_serializer.hpp>
 #include <optional>
-#include <string>
+#include <unordered_map>
 #include <vulkan/vulkan_core.h>
 
 namespace muon::schematic {
@@ -22,8 +20,8 @@ namespace muon::schematic {
 
     struct Pipeline {
         PipelineType type;
-        std::optional<PipelineState> state{std::nullopt}; // used by graphics and meshlet pipelines
-        std::map<ShaderStage, Shader> shaders{};
+        std::optional<PipelineStateInfo> state{std::nullopt}; // used by graphics and meshlet pipelines
+        std::unordered_map<ShaderStage, Shader> shaders{};
 
         [[nodiscard]] auto IsValid() const -> bool;
     };
@@ -36,30 +34,32 @@ namespace nlohmann {
 
     template<>
     struct adl_serializer<Pipeline> {
-        static auto to_json(json &j, const Pipeline &pipeline) {
-            j["type"] = static_cast<uint32_t>(pipeline.type);
+        static auto to_json(json &j, const auto &pipeline) {
+            j["type"] = magic_enum::enum_name(pipeline.type);
 
             for (const auto &[stage, shader] : pipeline.shaders) {
-                j["shaders"][fmt::format("{}", static_cast<uint32_t>(stage))] = shader;
+                j["shaders"][magic_enum::enum_name(stage)] = shader;
+            }
+
+            if (pipeline.type != PipelineType::Compute) {
+                j["state"] = pipeline.state;
             }
         }
 
-        static auto from_json(const json &j, Pipeline &pipeline) {
-            pipeline.type = j["type"].get<PipelineType>();
+        static auto from_json(const json &j, auto &pipeline) {
+            pipeline.type = *magic_enum::enum_cast<PipelineType>(j["type"].get<uint32_t>());
 
-            MU_CORE_ASSERT(j["shaders"].is_object(), "shaders must be a map");
-            for (const auto &[stage, shader] : j["shaders"].items()) {
-                try {
-                    pipeline.shaders[static_cast<ShaderStage>(std::stoi(stage))] = shader.get<Shader>();
-                } catch (const std::exception &e) {
-                    MU_CORE_ERROR("failed to parse {} to shader stage key");
+            if (j["shaders"].is_object()) {
+                for (const auto &[stage, shader] : j["shaders"].items()) {
+                    auto stageKey = magic_enum::enum_cast<ShaderStage>(stage);
+                    if (!stageKey.has_value()) { continue; }
+                    pipeline.shaders[*stageKey] = shader.get<Shader>();
                 }
             }
 
-            if (pipeline.type == PipelineType::Compute) { return; }
-            MU_CORE_ASSERT(j.contains("state"), "state must exist on graphics and meshlet pipelines");
-
-            auto state = j["state"];
+            if (pipeline.type != PipelineType::Compute && j.contains("state")) {
+                pipeline.state = j["state"].get<PipelineStateInfo>();
+            }
         }
     };
 
