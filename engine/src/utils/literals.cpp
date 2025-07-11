dc998b8c80c283a0c436eb3a78bc6361f4ef5546
Liam <lima.mail725@gmail.com>
2025-07-12 01:04:48 +1000

    fix: update literals to include 1024 multiples

--- engine/src/utils/literals.cpp
+++ engine/src/utils/literals.cpp
@@ -3,15 +3,27 @@
 namespace muon::literals {
 
     auto operator""_kb(unsigned long long value) -> size_t {
-        return value * 1'000;
+        return value * 1000;
     }
 
     auto operator""_mb(unsigned long long value) -> size_t {
-        return value * 1'000'000;
+        return value * 1000 * 1000;
     }
 
     auto operator""_gb(unsigned long long value) -> size_t {
-        return value * 1'000'000'000;
+        return value * 1000 * 1000 * 1000;
+    }
+
+    auto operator""_kib(unsigned long long value) -> size_t {
+        return value * 1024;
+    }
+
+    auto operator""_mib(unsigned long long value) -> size_t {
+        return value * 1024 * 1024;
+    }
+
+    auto operator""_gib(unsigned long long value) -> size_t {
+        return value * 1024 * 1024 * 1024;
     }
 
 }
