2a22124bce7901b6d18296248904a8a1e91a18ab
Liam <lima.mail725@gmail.com>
2025-07-12 21:40:05 +1000

    fix: reordered methods

--- engine/include/muon/asset/loader.hpp
+++ engine/include/muon/asset/loader.hpp
@@ -12,11 +12,11 @@ class Loader {
 public:
     virtual ~Loader() = default;
 
-    virtual auto FromMemory(const std::vector<uint8_t> &data) -> void = 0;
-    virtual auto FromFile(const std::filesystem::path &path) -> void = 0;
-
     [[nodiscard]] virtual auto GetFileType() -> std::string_view = 0;
     auto SetManager(Manager *manager) -> void { m_manager = manager; }
+
+    virtual auto FromMemory(const std::vector<uint8_t> &data) -> void = 0;
+    virtual auto FromFile(const std::filesystem::path &path) -> void = 0;
 
 protected:
     Manager *m_manager{nullptr};
