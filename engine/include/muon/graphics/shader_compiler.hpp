745516aaea9e4ed25b6196cd91e068da02f2a57c
Liam <lima.mail725@gmail.com>
2025-07-17 13:00:11 +1000

    feat: add shader compiler

---
+++ engine/include/muon/graphics/shader_compiler.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace muon::graphics {
+
+class ShaderCompiler {
+public:
+    ShaderCompiler();
+    ~ShaderCompiler();
+};
+
+} // namespace muon::graphics
---
+++ engine/src/graphics/shader_compiler.cpp
@@ -0,0 +1,9 @@
+#include "muon/graphics/shader_compiler.hpp"
+
+namespace muon::graphics {
+
+ShaderCompiler::ShaderCompiler() {}
+
+ShaderCompiler::~ShaderCompiler() {}
+
+} // namespace muon::graphics
