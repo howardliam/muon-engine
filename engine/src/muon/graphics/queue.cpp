dfc593239b4bc2637d585ffc6730f6c35db4ddc3
Liam <lima.mail725@gmail.com>
2025-07-24 02:51:00 +1000

    fix: update to use vulkan raii

--- engine/src/muon/graphics/queue.cpp
+++ engine/src/muon/graphics/queue.cpp
@@ -1,72 +1,61 @@
 #include "muon/graphics/queue.hpp"
 
-#include "muon/core/assert.hpp"
 #include "muon/core/log.hpp"
+#include "vulkan/vulkan_raii.hpp"
+#include "vulkan/vulkan_structs.hpp"
 
-#include <vulkan/vulkan_core.h>
+#include <vulkan/vulkan_enums.hpp>
 
 namespace muon::graphics {
 
-Queue::Queue(const Spec &spec) : m_device(spec.device), m_name(spec.name) {
-    vkGetDeviceQueue(m_device, spec.queueFamilyIndex, spec.queueIndex, &m_queue);
+Queue::Queue(const Spec &spec) : m_device(*spec.device), m_name(spec.name) {
+    m_queue = vk::raii::Queue{m_device, spec.queueFamilyIndex, spec.queueIndex};
 
-    VkCommandPoolCreateInfo poolInfo{VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO};
-    poolInfo.queueFamilyIndex = spec.queueFamilyIndex;
-    poolInfo.flags = VK_COMMAND_POOL_CREATE_TRANSIENT_BIT | VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
+    vk::CommandPoolCreateInfo commandPoolCi;
+    commandPoolCi.queueFamilyIndex = spec.queueFamilyIndex;
+    commandPoolCi.flags = vk::CommandPoolCreateFlagBits::eTransient | vk::CommandPoolCreateFlagBits::eResetCommandBuffer;
 
-    auto result = vkCreateCommandPool(m_device, &poolInfo, nullptr, &m_commandPool);
-    MU_CORE_ASSERT(result == VK_SUCCESS, "failed to create command pool");
+    m_commandPool = vk::raii::CommandPool{m_device, commandPoolCi};
 
     MU_CORE_DEBUG("created {} queue", m_name);
 }
 
-Queue::~Queue() {
-    vkDestroyCommandPool(m_device, m_commandPool, nullptr);
-    MU_CORE_DEBUG("destroyed {} queue", m_name);
-}
+Queue::~Queue() { MU_CORE_DEBUG("destroyed {} queue", m_name); }
 
-auto Queue::Wait() -> VkResult { return vkQueueWaitIdle(m_queue); }
+auto Queue::ExecuteCommands(std::function<void(vk::raii::CommandBuffer &commandBuffer)> const &recordFn) {
+    vk::CommandBufferAllocateInfo commandBufferAi;
+    commandBufferAi.level = vk::CommandBufferLevel::ePrimary;
+    commandBufferAi.commandPool = m_commandPool;
+    commandBufferAi.commandBufferCount = 1;
 
-auto Queue::BeginCommands() -> VkCommandBuffer {
-    VkCommandBufferAllocateInfo allocateInfo{VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO};
-    allocateInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
-    allocateInfo.commandPool = m_commandPool;
-    allocateInfo.commandBufferCount = 1;
+    vk::raii::CommandBuffers commandBuffers{m_device, commandBufferAi};
+    auto &commandBuffer = commandBuffers[0];
 
-    VkCommandBuffer commandBuffer;
-    auto result = vkAllocateCommandBuffers(m_device, &allocateInfo, &commandBuffer);
-    MU_CORE_ASSERT(result == VK_SUCCESS, "failed to allocate single time command buffer");
+    vk::CommandBufferBeginInfo commandBufferBi;
+    commandBufferBi.flags = vk::CommandBufferUsageFlagBits::eOneTimeSubmit;
+    commandBuffer.begin(commandBufferBi);
 
-    VkCommandBufferBeginInfo beginInfo{VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO};
-    beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+    recordFn(commandBuffer);
 
-    result = vkBeginCommandBuffer(commandBuffer, &beginInfo);
-    MU_CORE_ASSERT(result == VK_SUCCESS, "failed to begin command buffer recording");
+    commandBuffer.end();
 
-    return commandBuffer;
-}
-
-auto Queue::EndCommands(VkCommandBuffer cmd) -> void {
-    vkEndCommandBuffer(cmd);
-
-    VkSubmitInfo submitInfo{VK_STRUCTURE_TYPE_SUBMIT_INFO};
+    vk::SubmitInfo submitInfo;
     submitInfo.commandBufferCount = 1;
-    submitInfo.pCommandBuffers = &cmd;
+    submitInfo.pCommandBuffers = &(*commandBuffer);
 
-    auto result = vkQueueSubmit(m_queue, 1, &submitInfo, nullptr);
-    MU_CORE_ASSERT(result == VK_SUCCESS, "failed to submit command buffer to graphics queue");
+    m_queue.submit(submitInfo);
 
-    vkQueueWaitIdle(m_queue);
-
-    vkFreeCommandBuffers(m_device, m_commandPool, 1, &cmd);
+    m_queue.waitIdle();
 }
 
 auto Queue::GetFamilyIndex() const -> uint32_t { return m_familyIndex; }
 
 auto Queue::GetIndex() const -> uint32_t { return m_index; }
 
-auto Queue::Get() const -> VkQueue { return m_queue; }
+auto Queue::Get() -> vk::raii::Queue & { return m_queue; }
+auto Queue::Get() const -> const vk::raii::Queue & { return m_queue; }
 
-auto Queue::GetCommandPool() const -> VkCommandPool { return m_commandPool; }
+auto Queue::GetCommandPool() -> vk::raii::CommandPool & { return m_commandPool; }
+auto Queue::GetCommandPool() const -> const vk::raii::CommandPool & { return m_commandPool; }
 
 } // namespace muon::graphics
--- engine/src/muon/graphics/queue.hpp
+++ engine/src/muon/graphics/queue.hpp
@@ -2,16 +2,17 @@
 
 #include "muon/core/no_copy.hpp"
 #include "muon/core/no_move.hpp"
+#include "vulkan/vulkan_raii.hpp"
 
+#include <functional>
 #include <limits>
-#include <vulkan/vulkan_core.h>
 
 namespace muon::graphics {
 
 class Queue : NoCopy, NoMove {
 public:
     struct Spec {
-        VkDevice device{nullptr};
+        const vk::raii::Device *device{nullptr};
         uint32_t queueFamilyIndex{std::numeric_limits<uint32_t>().max()};
         uint32_t queueIndex{std::numeric_limits<uint32_t>().max()};
         const char *name{nullptr};
@@ -21,27 +22,27 @@ public:
     Queue(const Spec &spec);
     ~Queue();
 
-    [[nodiscard]] auto Wait() -> VkResult;
-
 public:
-    [[nodiscard]] auto BeginCommands() -> VkCommandBuffer;
-    auto EndCommands(VkCommandBuffer cmd) -> void;
+    auto ExecuteCommands(std::function<void(vk::raii::CommandBuffer &commandBuffer)> const &recordFn);
 
 public:
     auto GetFamilyIndex() const -> uint32_t;
     auto GetIndex() const -> uint32_t;
 
-    auto Get() const -> VkQueue;
-    auto GetCommandPool() const -> VkCommandPool;
+    auto Get() -> vk::raii::Queue &;
+    auto Get() const -> const vk::raii::Queue &;
+
+    auto GetCommandPool() -> vk::raii::CommandPool &;
+    auto GetCommandPool() const -> const vk::raii::CommandPool &;
 
 private:
-    VkDevice m_device{nullptr};
+    const vk::raii::Device &m_device;
     uint32_t m_familyIndex{std::numeric_limits<uint32_t>().max()};
     uint32_t m_index{std::numeric_limits<uint32_t>().max()};
     const char *m_name{nullptr};
 
-    VkQueue m_queue{nullptr};
-    VkCommandPool m_commandPool{nullptr};
+    vk::raii::Queue m_queue{nullptr};
+    vk::raii::CommandPool m_commandPool{nullptr};
 };
 
 } // namespace muon::graphics
