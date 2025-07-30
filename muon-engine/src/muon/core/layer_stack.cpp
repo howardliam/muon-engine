180c498da3b0082598cda5cd64c941f6fefc2b55
Liam <lima.mail725@gmail.com>
2025-07-30 21:05:20 +1000

    fix: detach layers for proper clean up

--- muon-engine/src/muon/core/layer_stack.cpp
+++ muon-engine/src/muon/core/layer_stack.cpp
@@ -8,6 +8,7 @@ LayerStack::LayerStack() { m_layerInsert = m_layers.begin(); }
 
 LayerStack::~LayerStack() {
     for (Layer *layer : m_layers) {
+        layer->onDetach();
         delete layer;
     }
 }
@@ -17,6 +18,7 @@ auto LayerStack::pushLayer(Layer *layer) -> void { m_layerInsert = m_layers.empl
 auto LayerStack::popLayer(Layer *layer) -> void {
     auto it = std::ranges::find(m_layers, layer);
     if (it != m_layers.end()) {
+        layer->onDetach();
         m_layers.erase(it);
         m_layerInsert -= 1;
     }
