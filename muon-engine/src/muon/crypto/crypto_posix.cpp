1f2d124f116f198d5d847bc84c2738cc4eaac516
Liam <lima.mail725@gmail.com>
2025-08-03 02:55:51 +1000

    refactor: remove unnecessary trace logs

--- muon-engine/src/muon/crypto/crypto_posix.cpp
+++ muon-engine/src/muon/crypto/crypto_posix.cpp
@@ -38,49 +38,40 @@ void loadFunctionPointers() {
     auto loadResult = utils::openDynamicLibrary("libcrypto.so");
     core::expect(loadResult, "failed to open library: libcrypto.so");
     g_libraryHandle = *loadResult;
-    core::trace("opened library: libcrypto.so");
 
     loadResult = utils::loadSymbol(g_libraryHandle, "OPENSSL_init_crypto");
     core::expect(loadResult, "failed to load symbol: OPENSSL_init_crypto");
     initCrypto = reinterpret_cast<InitCryptoFn>(*loadResult);
-    core::trace("loaded symbol: OPENSSL_init_crypto");
 
     initCrypto(0x00000008, nullptr);
 
     loadResult = utils::loadSymbol(g_libraryHandle, "SHA256");
     core::expect(loadResult, "failed to load symbol: SHA256");
     sha256 = reinterpret_cast<Sha256Fn>(*loadResult);
-    core::trace("loaded symbol: SHA256");
 
     loadResult = utils::loadSymbol(g_libraryHandle, "EVP_get_digestbyname");
     core::expect(loadResult, "failed to load symbol: EVP_get_digestbyname");
     getDigestByName = reinterpret_cast<GetDigestByNameFn>(*loadResult);
-    core::trace("loaded symbol: EVP_get_digestbyname");
 
     loadResult = utils::loadSymbol(g_libraryHandle, "EVP_MD_CTX_new");
     core::expect(loadResult, "failed to load symbol: EVP_MD_CTX_new");
     mdCtxNew = reinterpret_cast<MdCtxNewFn>(*loadResult);
-    core::trace("loaded symbol: EVP_MD_CTX_new");
 
     loadResult = utils::loadSymbol(g_libraryHandle, "EVP_MD_CTX_free");
     core::expect(loadResult, "failed to load symbol: EVP_MD_CTX_free");
     mdCtxFree = reinterpret_cast<MdCtxFreeFn>(*loadResult);
-    core::trace("loaded symbol: EVP_MD_CTX_free");
 
     loadResult = utils::loadSymbol(g_libraryHandle, "EVP_DigestInit_ex");
     core::expect(loadResult, "failed to load symbol: EVP_DigestInit_ex");
     digestInit = reinterpret_cast<DigestInitFn>(*loadResult);
-    core::trace("loaded symbol: EVP_DigestInit_ex");
 
     loadResult = utils::loadSymbol(g_libraryHandle, "EVP_DigestUpdate");
     core::expect(loadResult, "failed to load symbol: EVP_DigestUpdate");
     digestUpdate = reinterpret_cast<DigestUpdateFn>(*loadResult);
-    core::trace("loaded symbol: EVP_DigestUpdate");
 
     loadResult = utils::loadSymbol(g_libraryHandle, "EVP_DigestFinal_ex");
     core::expect(loadResult, "failed to load symbol: EVP_DigestFinal_ex");
     digestFinal = reinterpret_cast<DigestFinalFn>(*loadResult);
-    core::trace("loaded symbol: EVP_DigestFinal_ex");
 }
 
 Crypto::Crypto() {
