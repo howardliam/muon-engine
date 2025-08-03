7f9e8a2043a475f94f919f50c944b8d3c22f95b0
Liam <lima.mail725@gmail.com>
2025-08-03 12:10:33 +1000

    refactor: use libcrypto function names, allow oneshot hash to use other digests

--- muon-engine/src/muon/crypto/crypto_posix.cpp
+++ muon-engine/src/muon/crypto/crypto_posix.cpp
@@ -10,28 +10,25 @@ namespace muon::crypto {
 void *g_libraryHandle;
 
 using InitCryptoFn = int32_t (*)(uint64_t, void *);
-InitCryptoFn initCrypto;
-
-using Sha256Fn = uint8_t * (*)(const uint8_t *, size_t, uint8_t *);
-Sha256Fn sha256;
+InitCryptoFn OPENSSL_init_crypto;
 
 using GetDigestByNameFn = void * (*)(const char *);
-GetDigestByNameFn getDigestByName;
+GetDigestByNameFn EVP_get_digestbyname;
 
 using MdCtxNewFn = void * (*)();
-MdCtxNewFn mdCtxNew;
+MdCtxNewFn EVP_MD_CTX_new;
 
 using MdCtxFreeFn = void (*)(void *);
-MdCtxFreeFn mdCtxFree;
+MdCtxFreeFn EVP_MD_CTX_free;
 
 using DigestInitFn = int32_t (*)(void *, const void *, void *);
-DigestInitFn digestInit;
+DigestInitFn EVP_DigestInit_ex;
 
 using DigestUpdateFn = int32_t (*)(void *, const void *, size_t);
-DigestUpdateFn digestUpdate;
+DigestUpdateFn EVP_DigestUpdate;
 
 using DigestFinalFn = int32_t (*)(void *, uint8_t *, uint32_t *);
-DigestFinalFn digestFinal;
+DigestFinalFn EVP_DigestFinal_ex;
 
 void loadFunctionPointers() {
     auto openResult = utils::openDynamicLibrary("libcrypto.so");
@@ -40,37 +37,33 @@ void loadFunctionPointers() {
 
     auto loadResult = utils::loadSymbol(g_libraryHandle, "OPENSSL_init_crypto");
     core::expect(loadResult, "failed to load symbol: OPENSSL_init_crypto");
-    initCrypto = reinterpret_cast<InitCryptoFn>(*loadResult);
+    OPENSSL_init_crypto = reinterpret_cast<InitCryptoFn>(*loadResult);
 
-    initCrypto(0x00000008, nullptr);
-
-    loadResult = utils::loadSymbol(g_libraryHandle, "SHA256");
-    core::expect(loadResult, "failed to load symbol: SHA256");
-    sha256 = reinterpret_cast<Sha256Fn>(*loadResult);
+    OPENSSL_init_crypto(0x00000008, nullptr);
 
     loadResult = utils::loadSymbol(g_libraryHandle, "EVP_get_digestbyname");
     core::expect(loadResult, "failed to load symbol: EVP_get_digestbyname");
-    getDigestByName = reinterpret_cast<GetDigestByNameFn>(*loadResult);
+    EVP_get_digestbyname = reinterpret_cast<GetDigestByNameFn>(*loadResult);
 
     loadResult = utils::loadSymbol(g_libraryHandle, "EVP_MD_CTX_new");
     core::expect(loadResult, "failed to load symbol: EVP_MD_CTX_new");
-    mdCtxNew = reinterpret_cast<MdCtxNewFn>(*loadResult);
+    EVP_MD_CTX_new = reinterpret_cast<MdCtxNewFn>(*loadResult);
 
     loadResult = utils::loadSymbol(g_libraryHandle, "EVP_MD_CTX_free");
     core::expect(loadResult, "failed to load symbol: EVP_MD_CTX_free");
-    mdCtxFree = reinterpret_cast<MdCtxFreeFn>(*loadResult);
+    EVP_MD_CTX_free = reinterpret_cast<MdCtxFreeFn>(*loadResult);
 
     loadResult = utils::loadSymbol(g_libraryHandle, "EVP_DigestInit_ex");
     core::expect(loadResult, "failed to load symbol: EVP_DigestInit_ex");
-    digestInit = reinterpret_cast<DigestInitFn>(*loadResult);
+    EVP_DigestInit_ex = reinterpret_cast<DigestInitFn>(*loadResult);
 
     loadResult = utils::loadSymbol(g_libraryHandle, "EVP_DigestUpdate");
     core::expect(loadResult, "failed to load symbol: EVP_DigestUpdate");
-    digestUpdate = reinterpret_cast<DigestUpdateFn>(*loadResult);
+    EVP_DigestUpdate = reinterpret_cast<DigestUpdateFn>(*loadResult);
 
     loadResult = utils::loadSymbol(g_libraryHandle, "EVP_DigestFinal_ex");
     core::expect(loadResult, "failed to load symbol: EVP_DigestFinal_ex");
-    digestFinal = reinterpret_cast<DigestFinalFn>(*loadResult);
+    EVP_DigestFinal_ex = reinterpret_cast<DigestFinalFn>(*loadResult);
 }
 
 void init() {
@@ -83,39 +76,58 @@ void cleanup() {
 }
 
 auto hash(const uint8_t *data, size_t size) -> std::expected<std::array<uint8_t, k_hashSize>, CryptoError> {
+    const void *md = EVP_get_digestbyname("sha256");
+    void *ctx = EVP_MD_CTX_new();
+
+    int32_t result = EVP_DigestInit_ex(ctx, md, nullptr);
+    if (result == 0) {
+        return std::unexpected(CryptoError::InitializationFailure);
+    }
+
+    result = EVP_DigestUpdate(ctx, data, size);
+    if (result == 0) {
+        return std::unexpected(CryptoError::ProcessingFailure);
+    }
+
     std::array<uint8_t, k_hashSize> output;
-    sha256(data, size, output.data());
+    result = EVP_DigestFinal_ex(ctx, output.data(), nullptr);
+    if (result == 0) {
+        return std::unexpected(CryptoError::FinalizationFailure);
+    }
+
+    EVP_MD_CTX_free(ctx);
 
     return output;
 }
 
 auto hash(std::ifstream &file) -> std::expected<std::array<uint8_t, k_hashSize>, CryptoError> {
-    const void *md = getDigestByName("sha256");
-    void *ctx = mdCtxNew();
+    const void *md = EVP_get_digestbyname("sha256");
+    void *ctx = EVP_MD_CTX_new();
 
-    int32_t result = digestInit(ctx, md, nullptr);
+    int32_t result = EVP_DigestInit_ex(ctx, md, nullptr);
     if (result == 0) {
         return std::unexpected(CryptoError::InitializationFailure);
     }
 
+    // make sure to reset read position
     file.clear();
     file.seekg(0, std::ios::beg);
 
     std::string line;
     while (std::getline(file, line)) {
-        result = digestUpdate(ctx, reinterpret_cast<const uint8_t *>(line.data()), line.size());
+        result = EVP_DigestUpdate(ctx, reinterpret_cast<const uint8_t *>(line.data()), line.size());
         if (result == 0) {
             return std::unexpected(CryptoError::ProcessingFailure);
         }
     }
 
     std::array<uint8_t, k_hashSize> output;
-    result = digestFinal(ctx, output.data(), nullptr);
+    result = EVP_DigestFinal_ex(ctx, output.data(), nullptr);
     if (result == 0) {
         return std::unexpected(CryptoError::FinalizationFailure);
     }
 
-    mdCtxFree(ctx);
+    EVP_MD_CTX_free(ctx);
 
     // make sure to reset read position
     file.clear();
