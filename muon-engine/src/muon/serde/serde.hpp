82de13578dc90046ca3d683b704a80f24c0a509c
Liam <lima.mail725@gmail.com>
2025-08-07 18:52:59 +1000

    refactor!: use free templated functions

--- muon-engine/src/muon/serde/serde.hpp
+++ muon-engine/src/muon/serde/serde.hpp
@@ -6,13 +6,19 @@
 namespace muon::serde {
 
 template <typename Data, typename To>
+auto serialize(const Data &data) -> To;
+
+template <typename Data, typename To>
 concept Serializable = requires(const Data &data) {
-    { data.serialize() } -> std::same_as<To>;
+    { serialize<Data, To>(data) } -> std::same_as<To>;
 };
 
+template <typename From, typename Data, typename Error>
+auto deserialize(const From &from) -> std::expected<Data, Error>;
+
 template <typename Data, typename From, typename Error>
-concept Deserializable = requires(Data &data, const From &from) {
-    { Data::deserialize(from) } -> std::same_as<std::expected<Data, Error>>;
+concept Deserializable = requires(const From &from) {
+    { deserialize<From, Data, Error>(from) } -> std::same_as<std::expected<Data, Error>>;
 };
 
 }
