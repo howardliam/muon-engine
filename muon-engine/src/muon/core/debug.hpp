a1f3a5638b0082a8f8c687c99ea079b6414c0d6d
Liam <lima.mail725@gmail.com>
2025-08-10 12:42:02 +1000

    fix: new variable for migration

--- muon-engine/src/muon/core/debug.hpp
+++ muon-engine/src/muon/core/debug.hpp
@@ -5,10 +5,12 @@ namespace muon {
 #ifdef MU_DEBUG
 
 constexpr bool k_debugEnabled = true;
+constexpr bool debug_enabled = true;
 
 #else
 
 constexpr bool k_debugEnabled = false;
+constexpr bool debug_enabled = false;
 
 #endif
 
