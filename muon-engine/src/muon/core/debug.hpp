94916a5297c9ec81a44540b02d44e81c23dd68bf
Liam <lima.mail725@gmail.com>
2025-08-10 14:28:28 +1000

    fix: remove dead const

--- muon-engine/src/muon/core/debug.hpp
+++ muon-engine/src/muon/core/debug.hpp
@@ -3,15 +3,9 @@
 namespace muon {
 
 #ifdef MU_DEBUG
-
-constexpr bool k_debugEnabled = true;
 constexpr bool debug_enabled = true;
-
 #else
-
-constexpr bool k_debugEnabled = false;
 constexpr bool debug_enabled = false;
-
 #endif
 
 } // namespace muon
