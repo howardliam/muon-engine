d92d9813f7f99ddc05dc72bd18b14561bf251ca2
Liam <lima.mail725@gmail.com>
2025-08-10 22:01:28 +1000

    fix: formatting for includes

--- muon-engine/tests/main.cpp
+++ muon-engine/tests/main.cpp
@@ -1,8 +1,9 @@
 #include "muon/core/log.hpp"
 
-#include <cstdint>
 #define CATCH_CONFIG_RUNNER
-#include <catch2/catch_session.hpp>
+#include "catch2/catch_session.hpp"
+
+#include <cstdint>
 
 auto main(int32_t count, char **arguments) -> int32_t {
     muon::log::init();
--- muon-engine/tests/maths/alignment.cpp
+++ muon-engine/tests/maths/alignment.cpp
@@ -1,6 +1,6 @@
 #include "muon/maths/alignment.hpp"
 
-#include <catch2/catch_test_macros.hpp>
+#include "catch2/catch_test_macros.hpp"
 
 namespace muon {
 
