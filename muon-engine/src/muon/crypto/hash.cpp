cc488f31ce21799f046e50d45857a86bf44425a9
Liam <lima.mail725@gmail.com>
2025-08-10 12:39:24 +1000

    refactor!: STL-like syntax

--- muon-engine/src/muon/crypto/hash.cpp
+++ muon-engine/src/muon/crypto/hash.cpp
@@ -4,26 +4,26 @@
 
 namespace muon::crypto {
 
-auto hash(const uint8_t *data, size_t size) -> std::expected<std::array<uint8_t, 32>, HashError> {
-    std::array<uint8_t, 32> output;
+auto hash(const uint8_t *data, size_t size) -> std::expected<std::array<uint8_t, hash_size>, hash_error> {
+    std::array<uint8_t, hash_size> output;
     int32_t result = crypto_generichash(output.data(), output.size(), data, size, nullptr, 0);
     if (result != 0) {
-        return std::unexpected(HashError::ProcessingFailure);
+        return std::unexpected(hash_error::processing_failure);
     }
 
     return output;
 }
 
-auto hash(std::string_view string) -> std::expected<std::array<uint8_t, k_hashSize>, HashError> {
+auto hash(std::string_view string) -> std::expected<std::array<uint8_t, hash_size>, hash_error> {
     return hash(reinterpret_cast<const uint8_t *>(string.data()), string.size());
 }
 
-auto hash(std::ifstream &file) -> std::expected<std::array<uint8_t, 32>, HashError> {
+auto hash(std::ifstream &file) -> std::expected<std::array<uint8_t, hash_size>, hash_error> {
     crypto_generichash_state state;
 
-    int32_t result = crypto_generichash_init(&state, nullptr, 0, 32);
+    int32_t result = crypto_generichash_init(&state, nullptr, 0, hash_size);
     if (result != 0) {
-        return std::unexpected(HashError::InitializationFailure);
+        return std::unexpected(hash_error::initialization_failure);
     }
 
     // make sure to reset read position
@@ -34,7 +34,7 @@ auto hash(std::ifstream &file) -> std::expected<std::array<uint8_t, 32>, HashErr
     while (std::getline(file, line)) {
         result = crypto_generichash_update(&state, reinterpret_cast<const uint8_t *>(line.data()), line.size());
         if (result != 0) {
-            return std::unexpected(HashError::ProcessingFailure);
+            return std::unexpected(hash_error::processing_failure);
         }
     }
 
@@ -42,10 +42,10 @@ auto hash(std::ifstream &file) -> std::expected<std::array<uint8_t, 32>, HashErr
     file.clear();
     file.seekg(0, std::ios::beg);
 
-    std::array<uint8_t, 32> output;
+    std::array<uint8_t, hash_size> output;
     result = crypto_generichash_final(&state, output.data(), output.size());
     if (result != 0) {
-        return std::unexpected(HashError::FinalizationFailure);
+        return std::unexpected(hash_error::finalization_failure);
     }
 
     return output;
--- muon-engine/src/muon/crypto/hash.hpp
+++ muon-engine/src/muon/crypto/hash.hpp
@@ -8,17 +8,17 @@
 
 namespace muon::crypto {
 
-enum class HashError {
-    InitializationFailure,
-    ProcessingFailure,
-    FinalizationFailure,
+constexpr uint64_t hash_size = 32;
+
+enum class hash_error {
+    initialization_failure,
+    processing_failure,
+    finalization_failure,
 };
 
-constexpr uint64_t k_hashSize = 32;
+auto hash(const uint8_t *data, size_t size) -> std::expected<std::array<uint8_t, hash_size>, hash_error>;
+auto hash(std::string_view string) -> std::expected<std::array<uint8_t, hash_size>, hash_error>;
 
-auto hash(const uint8_t *data, size_t size) -> std::expected<std::array<uint8_t, k_hashSize>, HashError>;
-auto hash(std::string_view string) -> std::expected<std::array<uint8_t, k_hashSize>, HashError>;
-
-auto hash(std::ifstream &file) -> std::expected<std::array<uint8_t, k_hashSize>, HashError>;
+auto hash(std::ifstream &file) -> std::expected<std::array<uint8_t, hash_size>, hash_error>;
 
 } // namespace muon::crypto
