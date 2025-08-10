515c4a8f0d979df19b20d482d6f1cb6695da6603
Liam <lima.mail725@gmail.com>
2025-08-10 12:40:08 +1000

    fix: update to use layer properly

--- muon-engine/src/muon/core/layer_stack.cpp
+++ muon-engine/src/muon/core/layer_stack.cpp
@@ -7,20 +7,20 @@ namespace muon {
 layer_stack::layer_stack() { layer_insert_ = layers_.begin(); }
 
 layer_stack::~layer_stack() {
-    for (Layer *layer : layers_) {
-        layer->onDetach();
+    for (layer *layer : layers_) {
+        layer->on_detach();
         delete layer;
     }
 }
 
-void layer_stack::push_layer(Layer *layer) {
+void layer_stack::push_layer(layer *layer) {
     layer_insert_ = layers_.emplace(layer_insert_, layer);
 }
 
-void layer_stack::pop_layer(Layer *layer) {
+void layer_stack::pop_layer(layer *layer) {
     auto it = std::ranges::find(layers_, layer);
     if (it != layers_.end()) {
-        layer->onDetach();
+        layer->on_detach();
         layers_.erase(it);
         layer_insert_ -= 1;
     }
