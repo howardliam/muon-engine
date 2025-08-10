4ebaa1d17fa4da4886ed4e6b98833db0495a9360
Liam <lima.mail725@gmail.com>
2025-08-10 12:26:13 +1000

    feat: new variants to prepare for migration

--- muon-engine/src/muon/core/no_copy.hpp
+++ muon-engine/src/muon/core/no_copy.hpp
@@ -9,4 +9,11 @@ struct NoCopy {
     auto operator=(const NoCopy &other) -> NoCopy & = delete;
 };
 
+struct no_copy {
+    no_copy() = default;
+
+    no_copy(const no_copy &) = delete;
+    auto operator=(const no_copy &) -> no_copy & = delete;
+};
+
 } // namespace muon
--- muon-engine/src/muon/core/no_move.hpp
+++ muon-engine/src/muon/core/no_move.hpp
@@ -9,4 +9,11 @@ struct NoMove {
     auto operator=(NoMove &&other) -> NoMove & = delete;
 };
 
+struct no_move {
+    no_move() = default;
+
+    no_move(no_move &&) = delete;
+    auto operator=(no_move &&) -> no_move & = delete;
+};
+
 } // namespace muon
